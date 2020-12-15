95316aa7074c33797d6d0901620b2698da4207fa
Bond <977862185@qq.com>
2020-12-15 21:54:01 +0800

    1

---
+++ dota-user/dota-user-service/src/main/java/com/alex/dotauserservice/DotaUserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.alex.dotauserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class DotaUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotaUserServiceApplication.class, args);
+    }
+
+}
---
+++ dota-user/dota-user-service/src/test/java/com/alex/dotauserservice/DotaUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.dotauserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotaUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
