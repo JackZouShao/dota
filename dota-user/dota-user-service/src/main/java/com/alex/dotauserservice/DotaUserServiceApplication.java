04e9708cf10583be2d3d2479f1ac91408a5c0b20
Bond <977862185@qq.com>
2021-01-01 19:54:32 +0800

    1

--- dota-user/dota-user-service/src/main/java/com/alex/dotauserservice/DotaUserServiceApplication.java
+++ dota-user/dota-user-service/src/main/java/com/alex/dotauserservice/DotaUserServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.SpringCloudApplication;
 public class DotaUserServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DotaUserServiceApplication.class, args);
     }
 
