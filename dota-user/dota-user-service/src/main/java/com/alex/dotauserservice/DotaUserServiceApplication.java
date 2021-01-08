989807c257a51958bb6fb5ed6750a328921633b3
Bond <977862185@qq.com>
2021-01-08 20:38:42 +0800

    swagger

--- dota-user/dota-user-service/src/main/java/com/alex/dotauserservice/DotaUserServiceApplication.java
+++ dota-user/dota-user-service/src/main/java/com/alex/dotauserservice/DotaUserServiceApplication.java
@@ -1,15 +1,34 @@
 package com.alex.dotauserservice;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
 @SpringCloudApplication
-public class DotaUserServiceApplication {
+public class DotaUserServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
 
         SpringApplication.run(DotaUserServiceApplication.class, args);
     }
 
+
+
+//    @Value("${test}")
+//    private String test;
+//    @Value("${v}")
+//    private String v;
+//
+//    @Value("${x}")
+//    private String x;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+//        System.out.println(test);
+//        System.out.println(v);
+//        System.out.println(x);
+    }
 }
--- dota-user/dota-user-service/src/main/java/com/alex/dotauserservice/login/LoginController.java
+++ dota-user/dota-user-service/src/main/java/com/alex/dotauserservice/login/LoginController.java
@@ -1,8 +1,13 @@
 package com.alex.dotauserservice.login;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(value = "/login")
+@RestController
 public class LoginController {
+
 
 }
