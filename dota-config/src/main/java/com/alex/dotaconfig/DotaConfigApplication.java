00aa53d5bc99f59b68bbab51dda508bbafd2eafb
zy <zy@163.com>
2021-01-09 10:19:57 +0800

    1

--- dota-config/src/main/java/com/alex/dotaconfig/DotaConfigApplication.java
+++ dota-config/src/main/java/com/alex/dotaconfig/DotaConfigApplication.java
@@ -1,5 +1,7 @@
 package com.alex.dotaconfig;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
@@ -7,10 +9,19 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class DotaConfigApplication {
+public class DotaConfigApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(DotaConfigApplication.class, args);
     }
+    @Value("${test}")
+    private String test;
+    @Value("${v}")
+    private String v;
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(test);
+        System.out.println(v);
+    }
 }
