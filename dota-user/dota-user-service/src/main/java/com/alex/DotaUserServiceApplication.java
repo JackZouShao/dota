8554d4abe9d846d1f0f37f7d718f49fd30ea65d2
zy <zy@163.com>
2021-05-18 23:57:48 +0800

    【更新】配置中心

--- dota-user/dota-user-service/src/main/java/com/alex/DotaUserServiceApplication.java
+++ dota-user/dota-user-service/src/main/java/com/alex/DotaUserServiceApplication.java
@@ -8,9 +8,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringCloudApplication
 public class DotaUserServiceApplication {
     public static void main(String[] args) {
-//        SpringApplication.run(DotaUserServiceApplication.class, args);
         ConfigurableApplicationContext applicationContext = SpringApplication.run(DotaUserServiceApplication.class, args);
         String userName = applicationContext.getEnvironment().getProperty("user.name");
         String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        String nack = applicationContext.getEnvironment().getProperty("dota.nacos.discovery.server");;
+        System.out.println(userAge  + "-" + userName + "-" + nack + "-" );
     }
 }
