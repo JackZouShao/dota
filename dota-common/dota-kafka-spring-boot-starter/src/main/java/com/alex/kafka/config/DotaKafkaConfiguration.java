27eb74e384ab3d01b521541ad6d652cf11d51985
JSONZOU <jackzouyan@gmail.com>
2021-05-21 19:12:36 +0800

    【更新】kafka 集成

---
+++ dota-common/dota-kafka-spring-boot-starter/src/main/java/com/alex/kafka/config/DotaKafkaConfiguration.java
@@ -0,0 +1,68 @@
+package com.alex.kafka.config;
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+
+import java.util.Map;
+
+/**
+ * @version 1.0.0
+ * @className DotaKafkaConfiguration.java
+ * @author: yz
+ * @date: 2021/5/20 19:16
+ */
+@EnableConfigurationProperties({KafkaProperties.class})
+public class DotaKafkaConfiguration {
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<Integer, String>
+    kafkaListenerContainerFactory(ConsumerFactory<Integer, String> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Integer, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory(KafkaProperties kafkaProperties) {
+        Map<String, Object> consumerProperties = kafkaProperties.buildConsumerProperties();
+        return new DefaultKafkaConsumerFactory<>(consumerProperties);
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory(KafkaProperties kafkaProperties) {
+        return new DefaultKafkaProducerFactory<>(kafkaProperties.buildProducerProperties());
+    }
+
+    @Bean
+    public KafkaTemplate<Integer, String> kafkaTemplate(ProducerFactory<Integer, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+//    private Map<String, Object> consumerProps() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group");
+//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+//        // ...
+//        return props;
+//    }
+
+
+
+//    private Map<String, Object> senderProps() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        props.put(ProducerConfig.LINGER_MS_CONFIG, 10);
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        //...
+//        return props;
+//    }
+
+}
---
+++ soft/s.md
@@ -0,0 +1,4 @@
+### kafka
+```
+bin/zookeeper-server-start.sh config/zookeeper.properties
+```
