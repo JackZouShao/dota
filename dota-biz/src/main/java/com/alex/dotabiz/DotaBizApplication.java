292795bb87fdbe6c8d9a68b53594077b6581c2b7
ZouYan <977852185@qq.com>
2022-02-15 12:17:47 +0800

    init dota-biz

---
+++ dota-biz/src/main/java/com/alex/dotabiz/DotaBizApplication.java
@@ -0,0 +1,13 @@
+package com.alex.dotabiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotaBizApplication.class, args);
+    }
+
+}
---
+++ dota-biz/src/test/java/com/alex/dotabiz/DotaBizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.dotabiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotaBizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
