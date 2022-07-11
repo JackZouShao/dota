db7eb7c834f79682abeac8f3c8521918a5aa0eae
DESKTOP-B2EHQ3J\李逍遥 <977862185@qq.com>
2022-07-11 22:40:23 +0800

    Update: HELP & Oauth2ServerConfig.java

--- dota-auth/src/main/java/com/alex/auth/config/Oauth2ServerConfig.java
+++ dota-auth/src/main/java/com/alex/auth/config/Oauth2ServerConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import java.security.KeyPair;
